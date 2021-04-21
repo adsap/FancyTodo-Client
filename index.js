64be43550640a99f9bc482a63f244af71c395db5
adsap <addesaputra11@gmail.com>
2021-04-21 21:48:54 +0700

    login, logout, register, get todos

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 $(document).ready(() => {
   checkIsLoggedIn();
 
-  $('#formLogin').on('submit', (e) => {
+  $('#navRegister').click((e) => {
     e.preventDefault();
-    login();
+    $('#login').hide();
+    $('#register').show();
+    $('#todoList').hide();
+  });
+
+  $('#navLogin').click((e) => {
+    e.preventDefault();
+    $('#login').show();
+    $('#register').hide();
+    $('#todoList').hide();
   });
 
   $('#navLogout').click((e) => {
@@ -11,46 +20,62 @@ $(document).ready(() => {
     logout();
   });
 
+  $('#formLogin').on('submit', (e) => {
+    e.preventDefault();
+    login();
+  });
+
+  $('#formRegister').on('submit', (e) => {
+    e.preventDefault();
+    register();
+  });
+
+
 })
 
 const checkIsLoggedIn = () => {
   if (localStorage.getItem('access_token')) {
     $('#navLogin').hide();
     $('#navLogout').show();
+    $('#navRegister').hide();
+    $('#btn-add-todo').show();
 
     $('#login').hide();
-    $('#todos').show();
+    $('#register').hide();
+    $('#todoList').show();
 
     getTodos();
   } else {
     $('#navLogin').show();
     $('#navLogout').hide();
+    $('#navRegister').show();
 
     $('#login').show();
     $('#register').hide();
-    $('#todos').hide();
+    $('#todoList').hide();
+    $('#btn-add-todo').hide();
 
     clearTodos();
   }
 };
 
 const login = () => {
-  const inputEmailLogin = $('#inputEmailLogin');
-  const inputPasswordLogin = $('#inputPasswordLogin');
+  let email = $('#inputEmailLogin').val();
+  let password = $('#inputPasswordLogin').val();
 
   $.ajax({
     method: 'POST',
     url: 'http://localhost:3000/users/login',
     data: {
-      email: inputEmailLogin.val(),
-      password: inputPasswordLogin.val(),
-    },
+      email,
+      password,
+    }
   })
     .done((data) => {
       const { access_token } = data;
       localStorage.setItem('access_token', access_token);
-      inputEmailLogin.val('');
-      inputPasswordLogin.val('');
+      $('#inputEmailLogin').val("")
+      $('#inputPasswordLogin').val("");
     })
     .fail((err) => {
       const { errors } = err.responseJSON;
@@ -73,15 +98,20 @@ const getTodos = () => {
       access_token: localStorage.getItem('access_token'),
     },
   })
-    .done((data) => {
+    .done((todos) => {
       $('#todoList').empty();
-      data.data.forEach((todo) => {
+      todos.data.forEach((todo) => {
         $('#todoList').append(`
-        <div class="card-header text-center bg-info text-black">
-          Todos
+        <div class="card text-center mt-3 mx-auto" style="width: 18rem">
+          <div class="card-body">
+            <h5 class="card-title">${todo.title}</h5>
+            <p class="card-text">
+              ${todo.description}
+            </p>
+            <a href="#" class="btn btn-warning">Edit</a>
+            <a href="#" class="btn btn-danger">Delete</a>
+          </div>
         </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">${todo.title}</li>
         `);
       });
     })
@@ -93,4 +123,28 @@ const getTodos = () => {
 const clearTodos = () => {
   $('#todoList').empty();
 };
+
+const register = () => {
+  let email = $('#inputEmailRegister').val();
+  let password = $('#inputPasswordRegister').val();
+
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/users/register',
+    data: {
+      email,
+      password,
+    }
+  })
+    .done(() => {
+      $('#inputEmailRegister').val("")
+      $('#inputPasswordRegister').val("");
+    })
+    .fail((err) => {
+      const { errors } = err.responseJSON;
+    })
+    .always(() => {
+      checkIsLoggedIn();
+    });
+}
 
