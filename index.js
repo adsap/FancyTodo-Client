0e8212ec525ba45cbf9798435dbdb56628932728
adsap <addesaputra11@gmail.com>
2021-04-22 15:59:12 +0700

    create add, edit, delete

--- index.js
+++ index.js
@@ -30,6 +30,29 @@ $(document).ready(() => {
     register();
   });
 
+  $('#btn-add-todo').click((e) => {
+    e.preventDefault();
+    $('#btn-add-todo').hide();
+    $('#todoList').hide();
+    $('#add').show();
+  });
+
+  $('#formAddTodo').on('submit', (e) => {
+    e.preventDefault();
+    addTodo();
+    $('#btn-add-todo').show();
+    $('#todoList').show();
+    $('#add').hide();
+  });
+
+  $('#formEditTodo').on('submit', (e) => {
+    e.preventDefault();
+    editTodo();
+    $('#btn-add-todo').show();
+    $('#todoList').show();
+    $('#edit').hide();
+  });
+
 
 })
 
@@ -54,6 +77,8 @@ const checkIsLoggedIn = () => {
     $('#register').hide();
     $('#todoList').hide();
     $('#btn-add-todo').hide();
+    $('#add').hide();
+    $('#edit').hide();
 
     clearTodos();
   }
@@ -82,6 +107,7 @@ const login = () => {
     })
     .always(() => {
       checkIsLoggedIn();
+      $('#add').hide();
     });
 };
 
@@ -96,7 +122,7 @@ const getTodos = () => {
     url: 'http://localhost:3000/todos',
     headers: {
       access_token: localStorage.getItem('access_token'),
-    },
+    }
   })
     .done((todos) => {
       $('#todoList').empty();
@@ -108,8 +134,8 @@ const getTodos = () => {
             <p class="card-text">
               ${todo.description}
             </p>
-            <a href="#" class="btn btn-warning">Edit</a>
-            <a href="#" class="btn btn-danger">Delete</a>
+            <button type="button" class="btn btn-warning" onClick="getEditTodo(${todo.id})">Edit</button>
+            <button type="button" class="btn btn-danger" onClick="deleteTodo(${todo.id})">Delete</button>
           </div>
         </div>
         `);
@@ -117,6 +143,10 @@ const getTodos = () => {
     })
     .fail((err) => {
       const { errors } = err.responseJSON;
+    })
+    .always(() => {
+      $('#add').hide();
+      $('#edit').hide();
     });
 };
 
@@ -146,5 +176,124 @@ const register = () => {
     .always(() => {
       checkIsLoggedIn();
     });
+}
+
+const addTodo = () => {
+  let title = $('#inputTitle').val();
+  let description = $('#inputDescription').val();
+  let status = $('#inputStatus').val();
+  let due_date = $('#inputDueDate').val();
+
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/todos',
+    headers: {
+      access_token: localStorage.getItem('access_token'),
+    },
+    data: {
+      title,
+      description,
+      status,
+      due_date
+    }
+  })
+    .done(() => {
+      $('#inputTitle').val("");
+      $('#inputDescription').val("");
+      $('#inputStatus').val("");
+      $('#inputDueDate').val("");
+      getTodos()
+    })
+    .fail((err) => {
+      const { errors } = err.responseJSON;
+    })
+}
+
+const getEditTodo = (id) => {
+  $.ajax({
+    method: 'GET',
+    url: `http://localhost:3000/todos/${id}`,
+    headers: {
+      access_token: localStorage.getItem('access_token'),
+    }
+  })
+    .done((todo) => {
+      localStorage.setItem('todo-id', id);
+      let todoDate = new Date(todo.data.due_date)
+      $('#editTitle').val(todo.data.title);
+      $('#editDescription').val(todo.data.description);
+      $('#editStatus').val(todo.data.status);
+      $('#editDueDate').val(todoDate.toISOString().split('T')[0]);
+      $('#todoList').hide();
+      $('#edit').show();
+      $('#btn-add-todo').hide();
+    })
+    .fail((err) => {
+      const { errors } = err.responseJSON;
+    })
+}
+
+const editTodo = () => {
+  const id = localStorage.getItem('todo-id')
+  let title = $('#editTitle').val();
+  let description = $('#editDescription').val();
+  let status = $('#editStatus').val();
+  let due_date = $('#editDueDate').val();
+
+  $.ajax({
+    method: 'PUT',
+    url: `http://localhost:3000/todos/${id}`,
+    headers: {
+      access_token: localStorage.getItem('access_token'),
+    },
+    data: {
+      title,
+      description,
+      status,
+      due_date
+    }
+  })
+    .done(() => {
+      $('#editTitle').val("");
+      $('#editDescription').val("");
+      $('#editStatus').val("");
+      $('#editDueDate').val("");
+      getTodos()
+    })
+    .fail((err) => {
+      const { errors } = err.responseJSON;
+    })
+}
+
+const deleteTodo = (id) => {
+  $.ajax({
+    method: 'DELETE',
+    url: `http://localhost:3000/todos/${id}`,
+    headers: {
+      access_token: localStorage.getItem('access_token'),
+    }
+  })
+    .done(() => {
+      getTodos()
+    })
+    .fail((err) => {
+      const { errors } = err.responseJSON;
+    })
+}
+
+const cancel = () => {
+  $('#inputTitle').val("");
+  $('#inputDescription').val("");
+  $('#inputStatus').val("");
+  $('#inputDueDate').val("");
+  $('#editTitle').val("");
+  $('#editDescription').val("");
+  $('#editStatus').val("");
+  $('#editDueDate').val("");
+  $('#btn-add-todo').show();
+  $('#todoList').show();
+  $('#add').hide();
+  $('#edit').hide();
+  getTodos()
 }
 
